cac574058c715ff9c88d1afe3807700191000c6c
Petrlev <levendeevp@gmail.com>
2018-02-19 13:56:27 +0300

    Главная + robot txt

--- metaLinks.php
+++ metaLinks.php
@@ -1,6 +1,8 @@
 <meta name = "format-detection" content = "telephone=no" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
+<meta name="yandex-verification" content="1aed2a6f54207b68" />
+
 <link rel="stylesheet" type="text/css" href="/bootstrap/bootstrap.min.css" media="all" />
 <link rel="stylesheet" type="text/css" href="/style.css" media="all" />
 <link rel="stylesheet" type="text/css" href="/templates/toolsMarket/css/color_scheme_1/colorscheme.css" media="all" />
